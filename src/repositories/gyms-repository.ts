b168088657898d8637e009aeab519622ff63be9c
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2023-04-04 00:52:56 -0300

    Add search gums use case

--- src/repositories/gyms-repository.ts
+++ src/repositories/gyms-repository.ts
@@ -3,4 +3,5 @@ import { Gym, Prisma } from '@prisma/client'
 export interface GymsRepository {
   create(data: Prisma.GymCreateInput): Promise<Gym>
   findById(id: string): Promise<Gym | null>
+  searchManyByQuery(query: string, page: number): Promise<Gym[]>
 }
---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,26 @@
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { Gym } from '@prisma/client'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private checkInsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gyms = await this.checkInsRepository.searchManyByQuery(query, page)
+
+    return {
+      gyms,
+    }
+  }
+}
