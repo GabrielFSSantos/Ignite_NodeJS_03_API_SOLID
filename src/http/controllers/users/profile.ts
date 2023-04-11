07f2f94dad7a728184bf7d88f69b99557f0c6528
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2023-04-11 20:24:45 -0300

    Create routes to gyms and check-ins

--- src/http/controllers/users/profile.ts
+++ src/http/controllers/users/profile.ts
@@ -2,9 +2,11 @@ import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-p
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  const getUserProfile = makeGetUserProfileUseCase()
+  const getUserProfileUseCase = makeGetUserProfileUseCase()
 
-  const { user } = await getUserProfile.execute({ userId: request.user.sub })
+  const { user } = await getUserProfileUseCase.execute({
+    userId: request.user.sub,
+  })
 
   return reply.status(200).send({
     user: {
